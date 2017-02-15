5abcbb5f11cfb15767ca870a1fcaffa19151d9bd
Chris Gregori <chrisgreg23@googlemail.com>
2017-02-15 23:00:29 +0000

    Basic functionality with linked messages, emojis and authors

--- src/MadeWithLove.js
+++ src/MadeWithLove.js
@@ -2,10 +2,41 @@ import React, { Component } from 'react';
 
 export default class MadeWithLove extends Component {
 
+  getBaseMessage(props) {
+    const { emoji, by, link } = props;
+    let message = 'Made with <3';
+    let author = by;
+
+    if (emoji) {
+      message = 'Made with ❤️';
+    }
+
+    if (by) {
+      if (link) {
+        author = <a href={`${link}`}>{by}</a>;
+      }
+      return(
+        <span>
+          {`${message} by `}{author}
+        </span>
+      )
+    }
+
+    return(
+        <span>
+          {message}
+        </span>
+    );
+  }
+
+  componentWillMount() {
+    this.baseMessage = this.getBaseMessage(this.props);
+  }
+
   render() {
     return (
       <div className="made-with-love">
-        `Hello world`
+        {this.baseMessage}
       </div>
     )
   }
--- src/__tests__/MadeWithLove.test.js
+++ src/__tests__/MadeWithLove.test.js
@@ -8,5 +8,26 @@ describe('Made With Love', () => {
     const rendered = shallow(<MadeWithLove />);
     expect(rendered.find('.made-with-love').length).toBe(1);
   });
-  
+
+  it('Renders a base message without emojis', () => {
+    const rendered = shallow(<MadeWithLove />);
+    expect(rendered.text()).toBe('Made with <3');
+  });
+
+  it('Renders a base message with emojis', () => {
+    const rendered = shallow(<MadeWithLove emoji />);
+    expect(rendered.text()).toBe('Made with ❤️');
+  });
+
+  it('Renders a base message with an author', () => {
+    const rendered = shallow(<MadeWithLove by="Chris"/>);
+    expect(rendered.text()).toBe('Made with <3 by Chris');
+  });
+
+  it('Renders a base message with a linked author', () => {
+    const rendered = shallow(<MadeWithLove by="Chris" link="http://www.chrisgregori.co.uk"/>);
+    expect(rendered.find('a').length).toBe(1);
+    expect(rendered.text()).toBe('Made with <3 by Chris');
+  });
+
 })
